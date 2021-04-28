a464c3de2daeb0c450cfc91812a634450d2a271e
junya-takagi <junya10.1127@gmail.com>
2021-04-28 15:10:51 +0900

    W04

--- W06/main2.js
+++ W06/main2.js
@@ -30,7 +30,7 @@ function ShowScatterPlot(data){
         .attr("width",width)
         .attr("height",height)
         .append("g")
-        .attr("transform","translate(${margin.left},${margin.top}")
+        .attr("transform","translate(${margin.left},${margin.top})")
         .call(xaxis)
 
     svg.selectAll("circle")
