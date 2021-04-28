6e6a280d39711b98a029a418ae046ead35e37390
junya-takagi <junya10.1127@gmail.com>
2021-04-28 14:39:48 +0900

    W04

---
+++ W06/main2.js
@@ -0,0 +1,42 @@
+const width = 256;
+const height = 256;
+const margin = {top:10,right:10,bottom:10,left:10};
+
+d3.csv("https://junya-takagi.github.io/infovis2021/W06/data.csv")
+    .then(data =>{
+        data.forEach(d=>{d.x=+d.x;d.y=+d.y})
+        ShowScatterPlot(data);
+    })
+    .catch(error=>{
+        console.log(error);
+    });
+function ShowScatterPlot(data){
+    var svg = d3.select("body").append("svg").attr("width",width).attr("height",height)
+    svg.selectAll("circle")
+       .data(data)
+       .enter()
+       .append("circle")
+       .attr("cx",d=>xscale(d.x))
+       .attr("cy",d=>yscale(d.y))
+       .attr("r",d=>d.r)
+}
+
+var xscale = d3.scaleLinear()
+    .domain([d3.min(data,d=>d.x),d3.max(data,d=>d.x)])
+    .range([0,width-margin.left-margin.right])
+
+var yscale = d3.scaleLinear()
+    .domain([d3.min(data,d=>d.y),d3.max(data,d=>d.y)])
+    .range([0,height-top-bottom])
+
+var xaxis=d3.axisBottom(xscale)
+            .ticks(6)
+
+var svg = d3.select("body").append("svg")
+    .attr("width",width)
+    .attr("height",height)
+    .append("g")
+    .attr("transform","translate(${margin.left},${margin.top}")
+    .call(xaxis)
+
+
