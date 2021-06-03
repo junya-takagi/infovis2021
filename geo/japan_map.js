e082089df2f64753e9ef4d403d950617aa8de162
junya-takagi <junya10.1127@gmail.com>
2021-06-03 22:11:47 +0900

    geo

--- geo/japan_map.js
+++ geo/japan_map.js
@@ -1,18 +1,26 @@
-const width = 640;
-const height= 480;
-scale = 1500;
-d3.json("https://junya-takagi.github.io/infovis2021/geo/geojapan.geojson",createMap);
-function createMap(japan){
-    var projection = d3.geoMercator()
-        .translate([width/2,height/2])
-    var geoPath = d3.geoPath().projection(projection);
-    japan_map = d3.select("#japan_map").attr("width",width).attr("height",height);
 
-    var map = japan_map.selectAll("path").data(japan.features)
+var width = 600;
+var height = 600;
+var scale = 1600;
+d3.json("https://junya-takagi.github.io/infovis2021/geo/geojapan.geojson")
+.then(createMap);
+function createMap(japan){
+    var japan_map = d3.select("#japan_map").attr("width",width).attr("height",height);
+    var projection = d3.geoMercator()
+        .center([136.0, 35.6])
+        .translate([width/2,height/2])
+        .scale(scale)
+    var geoPath = d3.geoPath().projection(projection);
+    
+
+    japan_map.selectAll("path").data(japan.features)
         .enter()
         .append("path")
         .attr("d",geoPath)
+        .attr("id",function(d){
+            console.log(d.properties.name_local)
+        })
         .style("stroke","#ffffff")
         .style("stroke-width",0.1)
-        .style("fill","steelblue")
+        .style("fill","#5EAFC6");
 }
